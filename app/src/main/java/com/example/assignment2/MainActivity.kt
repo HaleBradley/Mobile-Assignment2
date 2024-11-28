4d891e6bde84053922ab6ba857f6ececfbff060a
HaleBradley <hbradley787@gmail.com>
2024-11-28 13:22:25 -0600

    assignment 6

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.assignment2
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -29,10 +33,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Assignment2Theme {
-                Scaffold("Assignment 2", "Main Activity") { MainContent(onClickExplicitly = { startSecondActivityExplicitly() }, onClickImplicitly = { startSecondActivityImplicitly() }, onClickImage = { startViewImageActivity() }) }
+
+        checkPermission {
+            setContent {
+                Assignment2Theme {
+                    Scaffold(
+                        "Assignment 2",
+                        "Main Activity"
+                    ) {
+                        MainContent(
+                            onClickExplicitly = { startSecondActivityExplicitly() },
+                            onClickImplicitly = { startSecondActivityImplicitly() },
+                            onClickImage = { startViewImageActivity() })
+                    }
+                }
             }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPermission(onGranted: () -> Unit) {
+        if(checkSelfPermission("com.example.assignment2.MSE412") == PackageManager.PERMISSION_GRANTED) {
+            onGranted()
+        } else {
+            requestPermissionLauncher.launch("com.example.assignment2.MSE412")
         }
     }
 
